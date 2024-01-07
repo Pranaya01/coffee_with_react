40ab2d06d558edeba3bc325a028c22b8a7f566b8
Pranaya01 <pradhanpranaya8@gmail.com>
2024-01-07 13:29:01 +0545

    React series-1

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>Coffee with React | Pranay Pradhan</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+   <h1>Coffee with Vite | Pranay Pradhan</h1>
+  )
+}
+
+export default App
